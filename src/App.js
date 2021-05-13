d648483230055913088ca89802f75e3bd16af507
bishalmallick <bishalmallick@gmail.com>
2021-05-13 19:35:34 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { FaTwitter, FaQuoteLeft } from 'react-icons/fa';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+
+class App extends Component {
+  constructor() {
+    super(); 
+      this.state = {
+        quotes: [],
+        index: 0
+      }
+      this.getRandomIndex = this.getRandomIndex.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(API).then(res => res.json()).then(res => {
+      this.setState({
+        quotes: res.quotes
+      });
+    });
+  }
+
+  getRandomIndex() {
+    const { quotes } = this.state;
+    if(quotes.length > 0) {
+      const index = Math.floor(Math.random() * quotes.length);
+      this.setState({
+        index
+      })
+    }
+  }
+  
+  render() {
+    const { quotes, index } = this.state;
+
+    const quote = quotes[index];
+
+  if(quotes.length > 0) {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>"Random Quote Machine"</h1>
+          <div className="quote-block">
+            <p className="quote"><FaQuoteLeft size="1.2em"/> {quote.quote}"</p>
+            <p className="author"><cite>- {quote.author}</cite></p>
+            <div className= "btn">
+              <button><a href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote.quote} - ${quote.author}`)} target="_blank" rel="noopener noreferrer"><FaTwitter size="25px"/></a></button>
+              <button onClick={this.getRandomIndex}>New Quote</button>
+            </div>
+          </div>
+          <div className="footer">
+            <footer>
+              <p className="footer__text">
+              © 2021 - Website developed by <a href="https://github.com/bishalmallick" target="_blank" rel="noopener noreferrer"> Bishal Mallick</a>
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return(<div className = "App-header">
+      <h1>Loading</h1>
+    </div>)
+  }
+}
 }
 
 export default App;
